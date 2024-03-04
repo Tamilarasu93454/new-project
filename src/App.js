99a73ff057690bc4e20e9dea90c7008866cfd975
Tamilarasu93454 <tamilarasushanmugavel@gmail.com>
2024-03-04 11:01:01 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,67 @@ import './App.css';
 import { createTheme, ThemeProvider } from '@mui/material';
 
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginPage from './components/login';
 import Dashboard from './components/dashboard';
 import Register from './components/register';
+import Tables from './components/tables';
+
+
+import { useState } from 'react';
 
 
 function App() {
   
 const theme = createTheme();
+
+    const [formData, setFormData] = useState({});
+    const [submittedData, setSubmittedData] = useState([]);
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      // Capture form data
+      const inputs = Array.from(e.target.elements);
+      const data = {};
+      inputs.forEach((input) => {
+        if (input.name) {
+          data[input.name] = input.value;
+        }
+      });
+      setSubmittedData([...submittedData, data]);
+      setFormData({}); // Clear form fields
+      alert("Your form is submitted"); 
+    };
+
+
+    ///
   return (
+    
+
+
+
     <ThemeProvider theme={theme}>
 
     <div className="App">
       <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path='/dashboard' element={<Dashboard/>} />
-            <Route path='/register' element={<Register /> } />
+           <Route path="/" element={<LoginPage />} />
+           <Route path='/dashboard' element={<Dashboard   
+                                              submittedData ={submittedData}
+                                              handleSubmit ={handleSubmit}
+                                              formData={formData}
+                                              setFormData ={setFormData}
+
+                                             />} />
+           {/* <Route path='/dashboard/tables' element={<Dashboard/>} /> */}
+           <Route path='/register' element={<Register /> } />
+           <Route path='/tables' element={<Tables
+                                           submittedData={submittedData}
+                                           setSubmittedData={setSubmittedData}
+                                           formData={formData}
+                                      
+                                          />} />
       </Routes>
       </Router>
     </div>
--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom'
 
 
 
-function Dashboard(){
+function Dashboard({ submittedData, handleSubmit ,formData,setFormData}){
   
   const navigate = useNavigate();
 
@@ -16,7 +16,10 @@ function Dashboard(){
     // For now, simply navigate to the login page
     navigate('/');
   };
-  
+  const handleDashboard=()=>{
+   navigate('/dashboard')
+
+  }
 
     const [isOpen1, setIsOpen1] = useState(false);                ////list opening
 
@@ -24,189 +27,155 @@ function Dashboard(){
     const toggleList1 = () => {
       setIsOpen1(!isOpen1);
     };
+    const handletables=()=>{                             //show the tables page
+      navigate('/tables');
+    }
 
-    const [formData, setFormData] = useState({});
-    const [submittedData, setSubmittedData] = useState([]);
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // Capture form data
-      const inputs = Array.from(e.target.elements);
-      const data = {};
-      inputs.forEach((input) => {
-        if (input.name) {
-          data[input.name] = input.value;
-        }
-      });
-      setSubmittedData([...submittedData, data]);
-      setFormData({}); // Clear form fields
-    };
-
-
+   
     return(
         <div>
- <header className='header'>
- 
+  <header className='header'>
+  
 
-     
+      
 
-    <Navbar style={{backgroundColor:"rgb(35, 127, 134)"}}>
-      <Container fluid className='Navbar-container'>
-         <h2 style={{fontWeight:'700', color:"white"}}>Administration</h2>
-         <div className='navbar-div'> 
-            <p >User</p>
-            <button onClick={handleLogout} className='logout-button'>
-                Logout
-              </button>
-         </div>
-      </Container>
-    </Navbar>
-     </header>
-
-
-
-     <Container fluid className="container-content  Dashboard-container">
-        <Row className="content-row">
-         <Col sm={2} className='content-col sidebar' style={{backgroundColorolor:"rgb(22, 41, 59)"}}>
+      <Navbar style={{backgroundColor:"rgb(35, 127, 134)"}}>
+        <Container fluid className='Navbar-container'>
+          <h2 style={{fontWeight:'700', color:"white"}}>Administration</h2>
+          <div className='navbar-div'> 
+              <p >User</p>
+              <button onClick={handleLogout} className='logout-button'>
+                  Logout
+                </button>
+          </div>
+        </Container>
+      </Navbar>
+      </header>
 
 
-    <div style={{backgroundColor:"rgb(35, 55, 74)"}}>
-      <div onClick={toggleList1} style={{ cursor: 'pointer', textDecoration: 'none',padding:'7px',color:'white'}}>
-        Categories
-      </div>     
 
-      {isOpen1 && (
-        <ul>
-          <li style={{listStyle:'none',color:'white',padding:'5px'}}>Employee</li>
-          <li style={{listStyle:'none',color:'white',padding:'5px'}}>Insights</li>
-          <li style={{listStyle:'none',color:'white',padding:'5px'}}>Profile information</li>
-          
-        </ul>
-      )}
-              </div> 
-         </Col>
+      <Container fluid className="container-content  Dashboard-container">
+          <Row className="content-row">
+          <Col sm={2} className='content-col sidebar' style={{backgroundColorolor:"rgb(22, 41, 59)"}}>
 
-         <Col  className='content-col'>
-          <h2>Employee Details</h2>
-          <form onSubmit={handleSubmit}>
-                <div className="input-box">
-                  <label style={{width:'130px'}} for='Name' >Name</label>
-                    <input
-                      type="text" // Change to password type for security
-                      name='Name'
-                      value={formData.Name || ''}
-                      onChange={(e) => setFormData({ ...formData, Name: e.target.value })}
-                    />
 
-              </div>
+      <div style={{backgroundColor:"rgb(35, 55, 74)"}}>
+        <div onClick={toggleList1} style={{ cursor: 'pointer', textDecoration: 'none',padding:'7px',color:'white'}}>
+          Categories
+        </div>     
+
+        {isOpen1 && (
+          <ul>
+            <li style={{listStyle:'none',color:'white',padding:'5px'}}>Employee</li>
+            <li style={{listStyle:'none',color:'white',padding:'5px'}}>Insights</li>
+            <li style={{listStyle:'none',color:'white',padding:'5px'}}>Profile information</li>
+            
+          </ul>
+        )}   
+              <div onClick={handletables} style={{ cursor: 'pointer', textDecoration: 'none',padding:'7px',color:'white'}}>
+              Tables
+              </div>  
+              <div onClick={handleDashboard} style={{ cursor: 'pointer', textDecoration: 'none',padding:'7px',color:'white'}}>
+              Dashboard
+             </div>    
+                
+         </div> 
+          </Col>
+
+          <Col  className='content-col'>
+            <h2>Employee Details</h2>
+            <form onSubmit={handleSubmit}>
                   <div className="input-box">
-                  
-                    <label for="email"  style={{width:'130px'}}>Email Address:</label>
-                    <input 
-                    type="email"
-                    id="email" 
-                    name="email"
-                    value={formData.email || ''}
-                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-                      /><br></br>
-                </div>
-
-                <div className="input-box">
-                        
-                  <label for="phone" style={{width:'130px'}}>Phone Number:</label>
-                  <input 
-                      type="tel" 
-                      id="phone"
-                      name="phone"
-                      value={formData.phone || ''}
-                      onChange={(e) => setFormData({ ...formData, phone: e.target.value })}
-                  /><br></br>
-                </div>
-
-                <div className="input-box">
-                  <label for="department"  style={{width:'130px'}}>Department:</label>
-                      <select id="department" name="department">
-                          <option value="marketing">Marketing</option>
-                          <option value="sales">Sales</option>
-                          <option value="finance">Finance</option>
-                      </select><br />
-
-                </div>
-       <div>
-            <label htmlFor="gender" style={{width:'130px'}}>Gender:</label>
-
-
-                  <input
-                        type="radio"
-                        id="male"
-                        name="gender"
-                        value="male"
-                        checked={formData.gender === "male"}
-                        onChange={(e) => setFormData({ ...formData, gender:"male"})}
+                    <label style={{width:'130px'}} for='Name' >Name</label>
+                      <input
+                        type="text" // Change to password type for security
+                        name='Name'
+                        value={formData.Name || ''}
+                        onChange={(e) => setFormData({ ...formData, Name: e.target.value })}
                       />
-            <label htmlFor="male" className='ms-2'>Male</label>
+
+                </div>
+                    <div className="input-box">
+                    
+                      <label for="email"  style={{width:'130px'}}>Email Address:</label>
+                      <input 
+                      type="email"
+                      id="email" 
+                      name="email"
+                      value={formData.email || ''}
+                      onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                        /><br></br>
+                  </div>
+
+                  <div className="input-box">
+                          
+                    <label for="phone" style={{width:'130px'}}>Phone Number:</label>
+                    <input 
+                        type="tel" 
+                        id="phone"
+                        name="phone"
+                        value={formData.phone || ''}
+                        onChange={(e) => setFormData({ ...formData, phone: e.target.value })}
+                    /><br></br>
+                  </div>
+
+                  <div className="input-box">
+                    <label for="department"  style={{width:'130px'}}>Department:</label>
+                        <select id="department" name="department">
+                            <option value="marketing">Marketing</option>
+                            <option value="sales">Sales</option>
+                            <option value="finance">Finance</option>
+                        </select><br />
+
+                  </div>
+        <div>
+              <label htmlFor="gender" style={{width:'130px'}}>Gender:</label>
+
+
+                    <input
+                          type="radio"
+                          id="male"
+                          name="gender"
+                          value="male"
+                          checked={formData.gender === "male"}
+                          onChange={(e) => setFormData({ ...formData, gender:"male"})}
+                        />
+              <label htmlFor="male" className='ms-2'>Male</label>
 
 
 
+                        <input
+                          type="radio"
+                          id="female"
+                          name="gender"
+                          value="female"
+                          checked={formData.gender === "female"}
+                          onChange={(e) => setFormData({ ...formData, gender:"female" })}
+                          className='ms-4'
+                    />
+              <label htmlFor="female" className='ms-'>Female</label>
+
+
+              
                       <input
                         type="radio"
-                        id="female"
+                        id="other"
                         name="gender"
-                        value="female"
-                        checked={formData.gender === "female"}
-                        onChange={(e) => setFormData({ ...formData, gender:"female" })}
+                        value="other"
+                        checked={formData.gender === "other"}
+                        onChange={(e) => setFormData({ ...formData, gender:"other" })}
                         className='ms-4'
-                  />
-            <label htmlFor="female" className='ms-'>Female</label>
+                      />
+              <label htmlFor="other" className='ms-2'>Other</label><br/>
+            </div>
+          
+      <input type="submit" value="Submit" className='my-3' ></input>
+      </form>
 
-
-            
-                    <input
-                      type="radio"
-                      id="other"
-                      name="gender"
-                      value="other"
-                      checked={formData.gender === "other"}
-                      onChange={(e) => setFormData({ ...formData, gender:"other" })}
-                      className='ms-4'
-                    />
-            <label htmlFor="other" className='ms-2'>Other</label><br/>
-           </div>
-        
-    <input type="submit" value="Submit" className='my-3'></input>
-    </form>
-
-        </Col>
-    </Row>
-    <Row>
-      <Col>
-          <div>
-          <h2>Submitted Details</h2>
-            <table style={{ borderCollapse: 'collapse', border: '1px solid black' }}>
-              <tbody>
-                <tr>
-                  <th style={{ width: '150px', border: '1px solid black' }}>Name</th>
-                  <th style={{ width: '190px', border: '1px solid black' }}>Email Address</th>
-                  <th style={{ width: '190px', border: '1px solid black' }}>Phone Number</th>
-                  <th style={{ width: '190px', border: '1px solid black' }}>Department</th>
-                  <th style={{ width: '190px', border: '1px solid black' }}>Gender</th>
-                </tr>
-                {submittedData.map((data, index) => (
-                  <tr key={index} className='py-3' style={{ border: '1px solid black', backgroundColor:'white' }}>
-                    <td style={{ border: '1px solid black', padding: '5px' }}>{data.Name}</td>
-                    <td style={{ border: '1px solid black', padding: '5px' }}>{data.email}</td>
-                    <td style={{ border: '1px solid black', padding: '5px' }}>{data.phone}</td>
-                    <td style={{ border: '1px solid black', padding: '5px' }}>{data.department}</td>
-                    <td style={{ border: '1px solid black', padding: '5px' }}>{data.gender}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-        </div> 
-      
-      </Col>
+          </Col>
       </Row>
-   </Container>
+  
+    </Container>
   </div>
     )
 }
